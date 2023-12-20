e2fb8191a1cf74237f83a17e7afbe2f7b9f7fd4f
Amaury <amaurydevelopper@gmail.com>
2023-12-20 13:13:56 +0100

    netlify file added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer.jsx";
 import Card from "../components/Card.jsx";
 import "../assets/banner/BannerHome.jpg";
 import "../style/Collapse.css";
-import "../style/Slideshow.css";
+import "../style/SlideShow.css";
 import "../style/Banner.css";
 import "../style/Card.css";
 import "../style/Footer.css";
